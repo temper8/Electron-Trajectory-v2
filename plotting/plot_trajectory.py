950fa516c8b18e4bf37f720efbd4cdd6c23759ca
Alex <temper8@gmail.com>
2026-05-07 11:57:13 +0300

    fix

--- plotting/plot_trajectory.py
+++ plotting/plot_trajectory.py
@@ -130,15 +130,12 @@ def get_envelope_fit(t_raw, x_raw):
     upper_env_func = interp1d(t_p, x_p, kind='linear', fill_value="extrapolate")
     lower_env_func = interp1d(t_t, x_t, kind='linear', fill_value="extrapolate")
 
-    upper_env = upper_env_func(t_raw)
-    lower_env = lower_env_func(t_raw)
+
 
     # 3. Расчет характеристик
     # Амплитуда A(t) — это полурасстояние между огибающими
-    A_t = (upper_env - lower_env) / 2
-
-    # Средняя линия (offset) — если центр колебаний смещен
-    offset_t = (upper_env + lower_env) / 2    
+    #A_t = (upper_env - lower_env) / 2
+ 
 
     # Частота w(t) — используем и пики, и впадины для лучшего разрешения
     t_all = np.sort(np.concatenate([t_p, t_t]))
@@ -147,14 +144,19 @@ def get_envelope_fit(t_raw, x_raw):
     w_vals = np.pi / half_periods 
     t_w = t_all[:-1] + half_periods / 2
     w_interp = interp1d(t_w, w_vals, kind='linear', fill_value="extrapolate")
-    w_t = w_interp(t_raw)
-    return lower_env, upper_env, offset_t, w_t
+    
+    w_t = w_interp(t_all)
+    upper_env = upper_env_func(t_all)
+    lower_env = lower_env_func(t_all)
+    # Средняя линия (offset) — если центр колебаний смещен
+    offset_t = (upper_env + lower_env) / 2   
+    return lower_env, upper_env, offset_t, w_t, t_all
 
 def plot_envelope_fit(df, a, title, race_name):
     x_raw = np.array(df['r'])/a
     t_raw = np.array(df['time'])
 
-    lower_env, upper_env, offset_t, w_t = get_envelope_fit(t_raw, x_raw)
+    lower_env, upper_env, offset_t, w_t, t_all = get_envelope_fit(t_raw, x_raw)
 
     # 3. Визуализация
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True, num=f"{race_name}_envelope_fit")
@@ -162,16 +164,16 @@ def plot_envelope_fit(df, a, title, race_name):
     # Верхний график: Сигнал и Огибающие
     ax1.plot(t_raw, x_raw, color='gray', alpha=0.3, label='r(t)/a')
     # Для огибающих используем линейную интерполяцию по найденным точкам
-    ax1.plot(t_raw, upper_env, 'r--', label='Верхняя огибающая')
-    ax1.plot(t_raw, lower_env, 'b--', label='Нижняя огибающая')
-    ax1.plot(t_raw, offset_t, 'k', label='Средняя линия (offset)', alpha=0.5)
+    ax1.plot(t_all, upper_env, 'r--', label='Верхняя огибающая')
+    ax1.plot(t_all, lower_env, 'b--', label='Нижняя огибающая')
+    ax1.plot(t_all, offset_t, 'k', label='Средняя линия (offset)', alpha=0.5)
     #ax1.fill_between(t_raw, lower_env, upper_env, color='yellow', alpha=0.1)
     ax1.set_ylabel('r(t)/a')
     ax1.legend()
     ax1.set_title(title)
 
     # Нижний график: Две частоты
-    ax2.plot(t_raw, w_t, 'r.-', label='w(t) по максимумам', alpha=0.7)
+    ax2.plot(t_all, w_t, 'r.-', label='w(t) по максимумам', alpha=0.7)
     #ax2.plot(t_w_upper, w_upper, 'r.-', label='w(t) по максимумам', alpha=0.7)
     #ax2.plot(t_w_lower, w_lower, 'b.-', label='w(t) по минимумам', alpha=0.7)
     ax2.set_ylabel('Частота w')
