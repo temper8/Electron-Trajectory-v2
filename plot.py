68481a4ada8e96b12cd871f3128fd09d0b1ac50f
Alex <temper8@gmail.com>
2026-05-08 10:54:14 +0300

    rename module

--- plot.py
+++ plot.py
@@ -32,9 +32,10 @@ init_env(tau_norm*ccc_R0, R0, a)
 from src.plot_environment import plot_field_environment
 from src.envelope_fit import plot_envelope_fit
 from src.plot_r_phi import plot_r_phi_segments
-from src.plot_trajectory import plot_poincare, plot_timeline_r_phi_poincare, plot_traj, polar_plot_traj
+from src.plotting import plot_poincare, plot_timeline_r_phi_poincare, plot_traj, polar_plot_traj
 
 
+#df = pd.read_hdf(race_file, 'trajectory', mode='r', start=0, stop=100000)
 df = pd.read_hdf(race_file, 'trajectory', mode='r')
 df['R'] = R0 + df['r']*cos(df['theta'])
 df['Z'] = df['r']*sin(df['theta'])
@@ -74,7 +75,7 @@ show()
 polar_plot_traj(df, a, race_file.stem)
 show()
 
-plot_timeline_r_phi_poincare(df, pp_df, a)
+plot_timeline_r_phi_poincare(df, pp_df, a, race_file.stem)
 show()
 
 plot_poincare(df, pp_df, race_file.stem)
