f33a2c75bdacd13bb148a2e7afbf7b145790c37e
Alex <temper8@gmail.com>
2026-05-12 11:36:19 +0300

    add fn initialize_particle_state

--- solver.py
+++ solver.py
@@ -37,22 +37,40 @@ init_env(tau_norm*ccc_R0, params.R0, params.a)
 
 from src.eqations import *
 
-t_ini = run_cfg.time_start*ccc_R0/tau_norm
+def initialize_particle_state(tau, params):
+    """
+    Инициализирует начальное состояние частицы и параметры магнитного поля.
+    """
+    
+    # Получение параметров окружения и расчет профиля safety factor
+    sf0, sfb, Uloop, B0 = get_field_environment(tau)
+    sf = saf_fact(sf0, sfb, params.r, params.a)
+    
+    # Расчет компонентов магнитного поля
+    (Btot, Btorini, Bpolini, Bpol1, Bradini, brad, btor, bpol, bpol1, 
+     dBpoldr, dBtordfi, dBraddr, dBtordr, dBpoldfi, dBraddfi,
+     dBpoldthet, dBtordthet, dBraddthet, dBpoldthet1, dBtordthet1, dBraddthet1, 
+     psitorini, dpsidr, dpsidfi) = Mag_field(params.r, params.theta, params.phi, tau, params)
+    
+    # Расчет инвариантов и начальной энергии
+    pperp2 = params.pperp**2    
+    mu = pperp2 / Btot
+    p2 = params.ppar**2 + pperp2
+    
+    # Расчет полоидального потока
+    psipol = pi * B0 * params.a**2 / (sfb - sf0) * log((sf0 + (sfb - sf0) * (params.r / params.a)**2) / sf0)
+    
+    # Энергия в эВ (или МэВ, в зависимости от констант)
+    energy = m01 * ccc1**2 * (sqrt(1 + p2) - 1) / 1.6022e-12
 
-t0c=t_ini
-sf0, sfb, Uloop, B0 = get_field_environment(t_ini)
-#print('t_ini=',t0c,'sf0=',sf0,'sfb=',sfb,'B0=',B0,'Uloop=',Uloop)
-sf=saf_fact(sf0,sfb,params.r,params.a)
-Btotini,Btorini,Bpolini,Bpol1,Bradini,brad,btor,bpol,bpol1,dBpoldr,dBtordfi,dBraddr,dBtordr,dBpoldfi,dBraddfi,  \
-dBpoldthet,dBtordthet,dBraddthet,dBpoldthet1,dBtordthet1,dBraddthet1,psitorini,dpsidr,dpsidfi \
-=Mag_field(params.r, params.theta, params.phi, t_ini, params)
-pperp2ini=params.pperp**2    
-muini=pperp2ini/Btotini
-p2ini=params.ppar**2+pperp2ini
-psipolini=pi*B0*params.a**2/(sfb-sf0)*log((sf0+(sfb-sf0)*(params.r/params.a)**2)/sf0)
-energyini=m01*ccc1**2*(sqrt(1+p2ini)-1)/1.6022e-12
+    # Возвращаем словарь со всеми рассчитанными значениями
+    return  Btot, mu, psipol, energy
 
-logger.info(f'rini= {params.r}, thetini={params.theta}, fiini={params.phi}, pparini= {params.ppar}, energyini= {energyini}')
+# Расчет нормированного начального времени
+tau = run_cfg.time_start * ccc_R0 / tau_norm
+Btot, muini, psipol, energy = initialize_particle_state(tau, params)
+
+logger.info(f'r= {params.r}, theta={params.theta}, phi={params.phi}, ppar= {params.ppar}, energy= {energy}')
 logger.info(f"---------------------- start -------------------------------")
 
 calculation_start_time = time.time()
@@ -64,7 +82,7 @@ with pd.HDFStore(race_folder/race_file, mode='w') as store:
     save_namedtuple(store, 'solver', solver)
     save_namedtuple(store, 'config', run_cfg)
     logger.info(f"Open the HDF5 file :  {file.name}")
-    tau_start = t_ini
+    tau_start = tau
     r = params.r
     theta = params.theta
     phi = params.phi
