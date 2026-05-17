3eba4c501f0f3118e1d7733dcb4fba55955353db
Alex <temper8@gmail.com>
2026-05-17 16:56:55 +0300

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -14,7 +14,7 @@ def plot_traj(df, pp_df, title, race_name):
     ax0.grid(True)
     ax0.axis('equal')
 
-    if pp_df:
+    if pp_df is not None:
         ax1.scatter(pp_df['R'], pp_df['Z'], c= pp_df['time'], cmap='plasma', alpha=0.05, edgecolors='none', s=8)
         ax1.set_title(f'Poincare ({len(pp_df)}) points')
         ax1.set_xlabel('R')
@@ -35,7 +35,7 @@ def polar_plot_traj(df, pp_df, a, race_name):
     ax0.grid(True)
     #ax.set_title("Electron trajectory in poloidal crossection", va='bottom')
     
-    if pp_df:
+    if pp_df is not None:
         ax1.scatter( pp_df['theta'], pp_df['r']/a, c= pp_df['time'], cmap='plasma', alpha=0.05, edgecolors='none', s=8)
         ax1.set_rmax(1)
         #ax.set_rticks([0.2, 0.4, 0.6, 0.8])  # Less radial ticks
